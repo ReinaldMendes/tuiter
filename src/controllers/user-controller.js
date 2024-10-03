a867d0aed05472c05b22f6e634562c529579df39
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-10-03 11:42:30 -0300

    feat follow

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -10,7 +10,6 @@ export const signup = async (req, res) => {
     });
     const token = jwtServices.generateAcessToken(user);
     res.json(token);
-    res.status(201).json(user);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -29,6 +28,78 @@ export const login = async (req, res) => {
         error: "Email os passaword incorrect",
       });
     }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+export const store = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const user = req.user._id;
+    const content = await User.create({
+      text,
+      user,
+    });
+    res.status(201).json(content);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const index = async (req, res) => {
+  try {
+    const content = await User.find().exec();
+    res.json(content);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const show = async (req, res) => {
+  try {
+    const content = await User.findById(req.params.id).exec();
+    res.json(content);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const user = req.user._id;
+    const content = await User.findByIdAndUpdate({
+      _id: req.params.id,
+      user,
+    }).exec();
+    if (!contente) {
+      res.json(content);
+    } else {
+      res.sendStatus(403);
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+export const destroy = async (req, resp) => {
+  try {
+    await User.findOneAndDelete({ user: req.user._id, _id: req.params }).exec();
+    resp.json();
+  } catch (error) {
+    resp.json(error);
+  }
+};
+
+export const followUnfollow = async (req, res) => {
+  try {
+    if (!req.user.following.includes(req.params.id)) {
+      req.user.following.push(req.params.id);
+    } else {
+      const index = req.user.following.indexOf(req.params.id);
+      req.user.following.splice(index, 1);
+    }
+    await req.user.save();
+    res.json;
   } catch (error) {
     res.status(400).send(error.message);
   }
