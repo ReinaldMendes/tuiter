98b0f9db4af25e0b7f1384b129651274a8cb80b6
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-10-08 08:50:15 -0300

    Update user-router.js

--- src/router/user-router.js
+++ src/router/user-router.js
@@ -1,21 +1,30 @@
 import { Router } from "express";
-import { signup, login } from "../controllers/user-controller.js";
-import jwtAuthenticator from "../middlewares/jwt-autenticator.js";
-import authorizer from "../middlewares/authorizer.js";
+import {
+  signup,
+  login,
+  store,
+  index,
+  update,
+  destroy,
+  follow,
+  unfollow,
+} from "../controllers/user-controller.js";
+import jwtAuthenticator from "../middlewares/jwt-autenticator.js"; // Middleware para autenticação JWT
+import authorizer from "../middlewares/authorizer.js"; // Middleware para autorização por role
+
 const router = Router();
-//crud usuario......
-router.post("/signup", signup /* Criar uma conta de usuario*/);
-router.use(authorizer("ADMISTRATOR"));
-router.post("/login", login /*Cria um token para o usuario */);
+
+router.post("/signup", signup);
+
+router.post("/login", login);
 
 router.use(jwtAuthenticator);
-router.use(authorizer("ADMISTRATOR"));
-router.post("/", store);
-router.put("/:id", update);
-router.delete("/:id", destroy);
-
-// crud usuário
+router.post("/follow/:id", follow);
+router.delete("/unfollow/:id", unfollow);
+router.use(authorizer("ADMINISTRATOR", "SUPORT"));
+router.get("/", index);
+router.post("/", store); // Criar usuário
+router.put("/:id", update); // Atualizar usuário
+router.delete("/:id", destroy); // Deletar usuário
 
 export default router;
-
-
